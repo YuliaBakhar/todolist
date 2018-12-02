b7252bafa3353c2f462b9b1e001299fa663b279e
YuliaBakhar <bakhar.yulia@gmail.com>
2018-12-02 21:08:29 +0300

    fix: animation

--- src/TodoListTask.jsx
+++ src/TodoListTask.jsx
@@ -63,56 +63,60 @@ class TodoListTask extends Component {
     const task = todos.find(el => el.id === match.params.todoId);
     return (
       <div>
-        <MyCard key={task.id}>
-          <Toolbar>
-            <Button onClick={this.props.onBackClick}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-              >
-                <path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z" />
-                <path fill="none" d="M0 0h24v24H0z" />
-              </svg>
-            </Button>
-            <div>Task: {task.text}</div>
-          </Toolbar>
-          <form>
-            <div>Description:</div>
-            <div className="todo-project">
-              <textarea
-                placeholder="Enter description"
-                className="task-description"
-                onChange={this.onChangeDescription}
-                defaultValue={task.description}
-              />
-              <Button onClick={this.onAddDescription}>Save</Button>
-            </div>
-          </form>
-          <div>Subtasks:</div>
-          <form onSubmit={this.onAddSubtask}>
-            <div className="todo-project">
-              <input
-                placeholder="Enter subtask"
-                className="task-input"
-                value={this.state.subtaskText}
-                onChange={this.onChange}
-              />
-              <Button onClick={this.onAddSubtask}>Add</Button>
-            </div>
-            <div>
-              {task.subtasks.map(subtask => (
-                <SubtaskCard key={subtask.id}>
-                  {subtask.text}
-                  <Button onClick={() => onDeleteSubtask(task.id, subtask.id)}>
-                    Remove
-                  </Button>
-                </SubtaskCard>
-              ))}
-            </div>
-          </form>
-        </MyCard>
+        {task && (
+          <MyCard key={task.id}>
+            <Toolbar>
+              <Button onClick={this.props.onBackClick}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z" />
+                  <path fill="none" d="M0 0h24v24H0z" />
+                </svg>
+              </Button>
+              <div>Task: {task.text}</div>
+            </Toolbar>
+            <form>
+              <div>Description:</div>
+              <div className="todo-project">
+                <textarea
+                  placeholder="Enter description"
+                  className="task-description"
+                  onChange={this.onChangeDescription}
+                  defaultValue={task.description}
+                />
+                <Button onClick={this.onAddDescription}>Save</Button>
+              </div>
+            </form>
+            <div>Subtasks:</div>
+            <form onSubmit={this.onAddSubtask}>
+              <div className="todo-project">
+                <input
+                  placeholder="Enter subtask"
+                  className="task-input"
+                  value={this.state.subtaskText}
+                  onChange={this.onChange}
+                />
+                <Button onClick={this.onAddSubtask}>Add</Button>
+              </div>
+              <div>
+                {task.subtasks.map(subtask => (
+                  <SubtaskCard key={subtask.id}>
+                    {subtask.text}
+                    <Button
+                      onClick={() => onDeleteSubtask(task.id, subtask.id)}
+                    >
+                      Remove
+                    </Button>
+                  </SubtaskCard>
+                ))}
+              </div>
+            </form>
+          </MyCard>
+        )}
       </div>
     );
   }
